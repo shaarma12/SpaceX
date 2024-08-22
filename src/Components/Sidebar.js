77a5ff48cfecba0e422a99693f5f5eeb29eaf2d6
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2024-08-23 00:47:33 +0530

    onclick make the value active

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
-  return (
-    <div className='w-[22%] bg-white rounded-tl-2xl rounded-bl-2xl'>
-    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/SpaceX_logo_black.svg/1920px-SpaceX_logo_black.svg.png" alt="logo" className='h-7 m-auto mt-20' />
-    <p className='font-thin text-sm tracking-[0.45em] ml-7 mt-1'>LIVE-INFO</p>
-    <div className='mt-24 text-[#656565] text-2xl ml-5'>
-      <Link to="/"><p className='hover:bg-[#f5f5f5] py-2 px-3 w-36 rounded-md'>Dashboard</p></Link>
-      <Link to="/rockets"><p className='hover:bg-[#f5f5f5] py-2 px-3 -mt-2 w-28 rounded-md'>Rockets</p></Link>
-    </div>
-    </div>
-  )
+    return (
+        <div className='w-[22%] bg-white rounded-tl-2xl rounded-bl-2xl'>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/SpaceX_logo_black.svg/1920px-SpaceX_logo_black.svg.png" alt="logo" className='h-7 m-auto mt-20' />
+            <p className='font-thin text-sm tracking-[0.45em] ml-7 mt-1'>LIVE-INFO</p>
+            <div className='mt-24 text-[#656565] text-2xl ml-5 flex flex-col'>
+                <NavLink to="/" className={({ isActive }) => isActive ? 'hover:bg-[#f5f5f5] py-2 px-3 w-36 rounded-md text-black font-semibold' : 'hover:bg-[#f5f5f5] py-2 px-3 w-36 rounded-md'}>Dashboard</NavLink>
+                <NavLink to='/rockets' className={({isActive})=>isActive?'hover:bg-[#f5f5f5] py-2 px-3 -mt-2 w-28 rounded-md text-black font-semibold':'hover:bg-[#f5f5f5] py-2 px-3 -mt-2 w-28 rounded-md'}>Rockets</NavLink>
+            </div>
+        </div>
+    )
 }
 
 export default Sidebar
