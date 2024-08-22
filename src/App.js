52e5f95defbae17520b76c713b8551da903fc99b
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2024-08-22 21:44:47 +0530

    creating the landing page structure

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <p className='text-red-700 font-bold'>Hello</p>
+    <div className='bg-[#EFEFEF] h-screen flex justify-center items-center'>
+        <div className='h-[40rem] w-[80rem] flex relative drop-shadow-2xl'>
+          <div className='w-[22%] bg-white rounded-tl-2xl rounded-bl-2xl'></div>
+          <div className='w-[80%] bg-[url("https://live.staticflickr.com/7706/26751237322_5a52540ea3_h.jpg")] bg-cente bg-120 rounded-tr-2xl rounded-br-2xl'>
+          </div>
+        </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundSize: {
+        '120': '120%',
+      },
+    },
   },
   plugins: [],
 }
