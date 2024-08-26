e5a2f9a28361fab8878df60d73b6fa2d6b1306c4
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2024-08-26 20:38:13 +0530

    photo carousel in popup

--- src/Components/Photos.js
+++ src/Components/Photos.js
@@ -1,56 +1,36 @@
 import React, { useState } from 'react';
+import left from "../Images/left.png";
+import right from "../Images/right.png";
+const Photos = ({photos}) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
 
-const Photos = () => {
-  // Array of image URLs
-  const images = [
-    "https://farm1.staticflickr.com/929/28787338307_3453a11a77_b.jpg",
-    "https://farm4.staticflickr.com/3955/32915197674_eee74d81bb_b.jpg",
-    "https://farm1.staticflickr.com/293/32312415025_6841e30bf1_b.jpg",
-    "https://farm1.staticflickr.com/623/23660653516_5b6cb301d1_b.jpg",
-    "https://farm6.staticflickr.com/5518/31579784413_d853331601_b.jpg",
-    "https://farm1.staticflickr.com/745/32394687645_a9c54a34ef_b.jpg"
-  ];
-
-  // State to keep track of the current image index
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  // Function to go to the previous image
-  const handlePrevClick = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
-  };
-
-  // Function to go to the next image
-  const handleNextClick = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
-  };
-
-  return (
-    <div className='mt-2'>
-      <div className='relative w-[48rem] h-[30rem] overflow-hidden rounded-xl mt-1 drop-shadow-2xl'>
-        <img
-          src={images[currentIndex]}
-          alt={`rocket-img-${currentIndex}`}
-          className='object-cover object-center w-full h-full opacity-90'
-        />
-
-        {/* Left arrow button */}
-        <button 
-          onClick={handlePrevClick}
-          className='absolute left-0 top-1/2 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 rounded-full'
-        >
-          &#8592;
-        </button>
-
-        {/* Right arrow button */}
-        <button 
-          onClick={handleNextClick}
-          className='absolute right-0 top-1/2 transform -translate-y-1/2 bg-black bg-opacity-50 text-white p-2 rounded-full'
-        >
-          &#8594;
-        </button>
-      </div>
-    </div>
-  );
+    return (
+        <div className='mt-2'>
+            <button
+                onClick={() => {
+                    setCurrentIndex((prevIndex) => prevIndex === 0 ? photos.length - 1 : prevIndex - 1);
+                }}
+                className='absolute left-16 top-1/2 transform -translate-y-1/2 p-2 hover:scale-y-105 transition-transform duration-200'
+            >
+                <img src={left} alt="leftArrow" />
+            </button>
+            <div className='relative w-[48rem] h-[30rem] overflow-hidden rounded-xl mt-1 drop-shadow-2xl'>
+                <img
+                    src={photos[currentIndex]}
+                    alt={`rocket-img-${currentIndex}`}
+                    className='object-cover object-center w-full h-full opacity-90 drop-shadow-2xl'
+                />
+            </div>
+            <button
+                onClick={() => {
+                    setCurrentIndex((prevIndex) => prevIndex === photos.length - 1 ? 0 : prevIndex + 1);
+                }}
+                className='absolute right-16 top-1/2 transform -translate-y-1/2 p-2 hover:scale-y-105 transition-transform duration-200'
+            >
+                <img src={right} alt="right" />
+            </button>
+        </div>
+    );
 };
 
 export default Photos;
