6d9a43785fa259dd33ce679c1dc16986d3bb22ae
Himanshu Sharma <50957983+shaarma12@users.noreply.github.com>
2024-09-02 23:09:52 +0530

    for tablets

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ const App = () => {
 
   return (
     <>
-    <div className='bg-[#EFEFEF] h-screen flex justify-center items-center'>
+    <div className='xl:bg-[#EFEFEF] lg:bg-[#EFEFEF]  h-screen flex justify-center items-center'>
         <div className='xl:h-[40rem] h-[35rem] w-[58rem] xl:w-[80rem] flex relative drop-shadow-2xl'>
-          <Sidebar />
-          <div className='xl:w-[80%] w-[80%] bg-[url("https://live.staticflickr.com/7706/26751237322_5a52540ea3_h.jpg")] bg-center bg-120 rounded-tr-2xl rounded-br-2xl'>
+            <Sidebar />
+          <div className='xl:w-[80%] lg:w-[80%] lg:bg-[url("https://live.staticflickr.com/7706/26751237322_5a52540ea3_h.jpg")] bg-center bg-120 rounded-tr-2xl rounded-br-2xl'>
             <Outlet />
           </div>
         </div>
